ac17835d8afc47832a279ecb2a630db2455ef21c
Owain Llewellyn <bilbercode@gmail.com>
2018-09-18 21:37:19 +0100

    feat: removed exporter

--- entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-nohup /nginx-vts-exporter -nginx.scrape_uri=http://localhost:8080/status/format/json -telemetry.address 0.0.0.0:8081 -telemetry.endpoint /__/metrics -metrics.namespace=$METRICS_NS &
-nginx
